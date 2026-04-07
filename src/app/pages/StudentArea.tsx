01818d4e68d9d1c39db7b461fdb062e38a242e52
Arewa Tolulope <tolulopearewa@student.oauife.edu.ng>
2026-04-07 02:46:09 +0100

    gg

--- src/app/pages/StudentArea.tsx
+++ src/app/pages/StudentArea.tsx
@@ -380,7 +380,7 @@ function Studentarea() {
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
               {loading || areaLoading ? (
-                [...Array(6)].map((_, i) => (
+                [...Array(3)].map((_, i) => (
                   <PropertyCardSkeleton key={i} />
                 ))
               ) : currentProperties.length === 0 ? (
