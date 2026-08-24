226530647f0b81f10ae90137fd2a3cec41bebc20
Arewa Tolulope <tolulopearewa@student.oauife.edu.ng>
2026-08-24 10:55:34 +0100

    fff

--- src/features/properties/pages/PropertyDetailsPage.tsx
+++ src/features/properties/pages/PropertyDetailsPage.tsx
@@ -940,7 +940,7 @@ function PropertyDetails() {
             {/* Pay Button */}
             <button
               onClick={() => setIsPaymentModalOpen(true)}
-              className="bg-white dark:bg-[#1A1A1A] border border-gray-600/30 hover:border-[#703BF7] hover:text-[#703BF7] text-gray-900 dark:text-white px-4 py-3.5 rounded-xl text-base font-medium transition-all flex items-center justify-center gap-2 cursor-pointer shadow-sm"
+              className="bg-white dark:bg-[#1A1A1A] border border-gray-600/30 hover:border-[#703BF7] hover:text-[#703BF7] text-gray-900 dark:text-white px-3 py-3.5 rounded-xl text-base font-medium transition-all flex items-center justify-center gap-1 cursor-pointer shadow-sm"
             >
               <FiCreditCard size={20} /> Pay for Property
             </button>
